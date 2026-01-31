eea90d8778487424d644ada2ab7c1f76dbf7d031
igred8 <ivangadjev@gmail.com>
2026-01-30 20:59:38 -0800

    J2 implemented

--- notebooks/00-particle_sim_basic_viz.py
+++ notebooks/00-particle_sim_basic_viz.py
@@ -3,6 +3,7 @@ import numpy.linalg as LA
 import scipy.constants as const
 from scipy.spatial.transform import Rotation
 
+import matplotlib.pyplot as plt
 import pyvista as pv
 
 import sys
@@ -21,20 +22,32 @@ if __name__ == "__main__":
     vel1 = 7904.76692
     mass1 = 100
 
+    npts = 7
     rng = np.random.default_rng( seed = 123 )
-    npts = 3000
-    dilate = rng.uniform(0.9, 1.3, npts)
-    thetas = rng.uniform(0, 2*const.pi, npts)
-    velang = np.deg2rad( rng.uniform( 0, 30, npts ))
+
+    rdilate = rng.uniform(0.9, 1.3, [npts,1])
+    phi = const.pi * rng.uniform(0, 2, [npts,1])
+    theta = const.pi * rng.uniform(0.25, 0.55, [npts,1])
+    velang = np.deg2rad( rng.uniform( 0, 3, [npts,1] ))
     
+    rvec = rdilate * (
+          np.cos(phi)*np.sin(theta)*XHAT
+        + np.sin(phi)*np.sin(theta)*YHAT
+        + np.cos(theta)*ZHAT
+        )
+    thvec = (
+          -np.sin(phi)*np.sin(theta)*XHAT
+        + np.cos(phi)*np.sin(theta)*YHAT
+        + np.cos(theta)*ZHAT
+        )
+
     sats = []
-    for dil,th,va in zip(dilate,thetas, velang):
-        rhat_ = np.cos(th)*XHAT + np.sin(th)*YHAT
+    for r,th,va in zip(rvec, thvec, velang):
         rot = Rotation.from_rotvec( va * ZHAT )
         sats.append(
             sat.GravityObject( 
-                xyz_m = dist1 * dil* rhat_,
-                vxyz_mps = rot.as_matrix() @ np.transpose(vel1 * LA.cross( rhat_, -ZHAT )),
+                xyz_m = dist1 * r,
+                vxyz_mps = rot.as_matrix() @ (vel1*th),
                 mass_kg=mass1
             )
         )
@@ -43,9 +56,9 @@ if __name__ == "__main__":
     for s in sats:
         esys.add_object( s )
 
-    esys.init_snapshot()
-    esys.calc_init_states()
-    for i in range(1000):
+    esys.init_snapshots( n_time_steps=2 )
+    
+    for i in range(20000):
         esys.propagate_step()
         
     r = np.stack(esys.positions)
@@ -54,8 +67,8 @@ if __name__ == "__main__":
 
     plotter = pv.Plotter()
     cloud = pv.PolyData(r[0] / sat.EARTH_RADIUS_M)
-    cloud['dilate'] = dilate
-    plotter.add_mesh(cloud, render_points_as_spheres=True, point_size=6, cmap='berlin')
+    cloud['rdilate'] = rdilate
+    plotter.add_mesh(cloud, render_points_as_spheres=True, point_size=6, cmap='viridis')
     # plotter.enable_eye_dome_lighting()
     plotter.open_movie("orbits.mp4", framerate=30)
     # plotter.show()
@@ -65,3 +78,17 @@ if __name__ == "__main__":
         # plotter.render()
         plotter.write_frame(  )
     plotter.close()
+
+    rnorm = LA.norm(r, ord=2, axis=2)
+    vnorm = LA.norm(v, ord=2, axis=2)
+
+    energy_gravity = np.sum( const.G * sat.EARTH_MASS_KG * esys.masses.T / rnorm, axis=1 )
+    energy_kinetic = 0.5 * np.sum( esys.masses.T * vnorm**2, axis=1 )
+
+    print(energy_gravity.shape)
+    print(energy_kinetic.shape)
+
+    fig, ax = plt.subplots(1,1)
+
+    ax.plot( energy_kinetic[2:]+energy_gravity[2:])
+    plt.show()  
--- src/satellites.py
+++ src/satellites.py
@@ -9,6 +9,9 @@ class GravityObject():
         self.mass = mass_kg
 
 
+# use GM_factor factor in gravity calcs as it is much more accurately known than either G or M_Earth
+GM_factor = 3.98600441889e14 # m^3 s^-2
+
 EARTH_MASS_KG = 5.9722e24
 EARTH_RADIUS_M = 6378137.0
 MOON_MASS_KG = 7.346e22
@@ -27,7 +30,25 @@ class EarthSystem():
     def add_object(self, gravobj:GravityObject ):
         self.objects.append(gravobj)
     
-    def init_snapshot(self):
+    def init_snapshots(self, n_time_steps:int=2):
+        """Calculate the state of the system in the first two time steps. 
+        Needed to get the Verlet integration going.
+
+        Raises
+        ------
+        ValueError
+            Needs there to have been objects defined for the class instance.
+
+        Parameters
+        ----------
+        n_time_steps : int, optional
+            Number of time steps to initialize. Need 2 steps if using Verlet, by default 2
+
+        Returns
+        -------
+        int
+            0 - completed, 1 - failed
+        """
         if not self.objects:
             return 1
         
@@ -37,42 +58,87 @@ class EarthSystem():
         self.accels = [np.zeros( [nobj, 3] )]
         self.masses = np.zeros( [nobj, 1] )
 
+        # init the snapshot arrays from all objects
         for i,o in enumerate(self.objects):
             self.positions[0][i,:] = np.array( o.position )
             self.velocities[0][i,:] = np.array( o.velocity )
-            self.accels[0][i,:] = self.calc_gravity_accel( o.position )
             self.masses[i,:] = np.array( o.mass )
-
+        self.accels[0] = self.calc_gravity_accel( self.positions[0] )
+        
+        # init the following steps with forward Euler
+        for i in range(n_time_steps-1):
+            ri, vi, ai = self._forward_euler( self.positions[-1], self.velocities[-1], self.accels[-1] )
+            self.add_snapshot( ri, vi, ai )
         return 0
     
     def add_snapshot( self, pos_new, vel_new, accel_new ):
+        """Append the given arrays as new elements in the lists.
+
+        Parameters
+        ----------
+        pos_new : ndarray
+            XYZ positions 
+        vel_new : ndarray
+            Velocities 
+        accel_new : ndarray
+            Accelerations
+
+        Raises
+        ------
+        ValueError
+            Needs the position, velocity, and acceleration arrays to have been initialized, typically using the `calc_init_states` method.
+        """
         if (self.positions is None) or (self.velocities is None):
-            raise ValueError("Please initialize the position and velocity arrays for the objects.")
+            raise ValueError("Please initialize the position and velocity arrays for the objects. Typically done with `calc_init_states`.")
         self.positions.append( pos_new )
         self.velocities.append( vel_new )
         self.accels.append( accel_new )
 
     def calc_gravity_accel(self, xyz ):
-        distances = LA.norm( xyz, ord=2, axis=1 ).reshape([-1,1])
-        grav_accel = -const.G * EARTH_MASS_KG * xyz / distances**3
-        return grav_accel
+        """With Earth as the center of the gravitational well, calculate the acceleration at the given points.
         
-    def calc_init_states( self ):
-        no_objects_present = self.init_snapshot()
-        if no_objects_present: 
-            raise ValueError("No objects present. Exiting time-step propagation.")
-            
-        a0 = self.accels[0]
-        v0 = self.velocities[0]
-        r0 = self.positions[0]
+        No corrections due to oblateness of Earth.
 
+        Parameters
+        ----------
+        xyz : ndarray
+            [N x 3] array of the XYZ coordinates of the points at which to calculate the acceleration due to gravity.
+
+        Returns
+        -------
+        ndarray
+            [N x 3] array of the vectors of the acceleration at the given points.
+        """
+        distances = LA.norm( xyz, ord=2, axis=1 ).reshape([-1,1])
+        r_unit = xyz / distances
+        grav_accel_00 = (-GM_factor / distances**2) * r_unit
+
+        J2 = 0.00162
+        x_ = xyz[:,0].reshape([-1,1])
+        y_ = xyz[:,1].reshape([-1,1])
+        z_ = xyz[:,2].reshape([-1,1])
+        grav_accel_20x = -GM_factor*EARTH_RADIUS_M*J2 * (x_ / distances**7) * (5*z_ - distances**2)
+        grav_accel_20y = -GM_factor*EARTH_RADIUS_M*J2 * (y_ / distances**7) * (5*z_ - distances**2)
+        grav_accel_20z = -GM_factor*EARTH_RADIUS_M*J2 * (z_ / distances**7) * (5*z_ - 3*distances**2)
+
+        grav_accel_20 = np.array( [grav_accel_20x, grav_accel_20y, grav_accel_20z] ).reshape([-1,3])
+        return grav_accel_00 + grav_accel_20
+        
+    def _forward_euler( self, r0,v0,a0 ):
+        """Make a step in time using the forward Euler integrator. Should just be used to init snapshots.
+        
+        """
+            
         r1 = r0 + v0*self.delta_time_s + 0.5*a0*self.delta_time_s**2
         v1 = v0 + a0*self.delta_time_s
         a1 = self.calc_gravity_accel( r1 )
 
-        self.add_snapshot( r1, v1, a1 )
+        return r1, v1, a1
 
     def propagate_step( self ):
+        """Calculate the position, velocity, and acceleration of the objects for the next time-step, using the velocity Verlet integration.
+        Append the arrays by calling `add_snapshot`
+        """
         # Velocity Verlet step
         r_new = self.positions[-1] + self.velocities[-1]*self.delta_time_s + 0.5*self.accels[-1]*self.delta_time_s**2
         a_new = self.calc_gravity_accel( r_new )
