1795d7b009fc649dea12983c694ee5cfd5e65c44
igred8 <ivangadjev@gmail.com>
2026-02-20 07:12:06 -0800

    refactored update steps outside of calc steps

--- src/stateest.py
+++ src/stateest.py
@@ -20,6 +20,13 @@ class KalmanFilter():
         self.R = R
         self.K = []
 
+    def update_xPuK(self, xnew, Pnew, unew, Knew):
+        """Append the new matricies for x, P, u, K.
+        """
+        self.x.append(xnew)
+        self.P.append(Pnew)
+        self.u.append(unew)
+        self.K.append(Knew)
 
     def model_predition_step(self, u=None):
         """Given a state vector and how it transitions in a single time-step based on a model, that may include the controls, predict the next state.
@@ -34,6 +41,16 @@ class KalmanFilter():
 
         P - convariance matrix of the state
         Q - convariance matrix of noisy controls
+
+        Parameters
+        ----------
+        u : ndarray, optional
+            [n_controls x 1] state of the controls, by default None
+
+        Returns
+        -------
+        xnew, Pnew, u : ndarray, ndarray, ndarray
+            _description_
         """
         xcurrent = self.x[-1]
         self.u.append(u)
@@ -47,9 +64,9 @@ class KalmanFilter():
         Pnew = self.F @ Pcurrent @ self.F.T + self.Q
         self.P.append(Pnew)
 
-        return xnew, Pnew
+        return xnew, Pnew, u
 
-    def measurement_update_step(self, z, postcovkwargs:dict={}):
+    def measurement_update_step(self, z=None, postcovkwargs:dict={}):
         """Updates the state estimate based on a measurement.
 
         x(t) = x(t-1) + K(t) . (z(t) - H(t).x(t-1))
@@ -74,19 +91,22 @@ class KalmanFilter():
         xnew, Pnew : ndarray, ndarray
         """
         xcurrent = self.x[-1]
-        # Pcurrent = self.P[-1]
+        Pcurrent = self.P[-1]
         
-        K = self.calc_kalman_gain() # updates the Kalman gain
+        K = self.calc_kalman_gain(Pcurrent)
+        # no measurement, only K might have updated
+        if z is None:
+            return xcurrent, Pcurrent, K
         
         xnew = xcurrent + K @ (z - self.H @ xcurrent)
-        Pnew = self.calc_posterior_covariance(**postcovkwargs)
+        Pnew = self.calc_posterior_covariance(Pcurrent, K, **postcovkwargs)
 
-        self.x.append(xnew)
-        self.P.append(Pnew)
+        # self.x.append(xnew)
+        # self.P.append(Pnew)
 
-        return xnew, Pnew
+        return xnew, Pnew, K
 
-    def calc_posterior_covariance(self, stable:bool=True):
+    def calc_posterior_covariance(self, P, K, stable:bool=True):
         """The posterior covariance in the form:
         P(t) = ( I - K(t).H(t) ) . P(t)
         can become unstable over many iterations due to accumulation of floating point math errors. This stems from the subtraction in the parentheses, where K and/or H may have very small values for some elements. This could lead to non-symmetric P, but P is a covariance matrix and must be symmetric.
@@ -97,9 +117,9 @@ class KalmanFilter():
         P = (I - K.H).P.(I - K.H)^T + K.R.K^T
 
         """
-        P = self.P[-1]
+        # P = self.P[-1]
         I = np.eye(*P.shape) # eye needs shape tuple unpacked
-        K = self.K[-1]
+        # K = self.K[-1]
         A = I - K @ self.H
 
         if stable:
@@ -110,17 +130,42 @@ class KalmanFilter():
         return Pnew
 
 
-    def calc_kalman_gain(self):
+    def calc_kalman_gain(self, P):
         """Calculate the Kalman gain, K, which specifies the optimal ratio of process vs. measurement estimate.
 
             K = P(t).H(t)^T . (H(t).P(t).H(t)^T + R(t))^-1
         """
-        Pcurrent = self.P[-1]
-        S_ = np.linalg.inv(self.H @ Pcurrent @ self.H.T + self.R)
-        K = Pcurrent @ self.H.T @ S_
-        self.K.append(K)
+        
+        S_ = np.linalg.inv(self.H @ P @ self.H.T + self.R)
+        K = P @ self.H.T @ S_
+        # self.K.append(K)
         
         return K
+
+    def perform_epoch(self, u=None, z=None, postcovkwargs=None):
+        """_summary_
+
+        Parameters
+        ----------
+        u : ndarray, optional
+            _description_, by default None
+        z : ndarray, optional
+            _description_, by default None
+        postcovkwargs : dict, optional
+            _description_, by default None
+
+        Returns
+        -------
+        ndarray, ndarray
+            state and state covariance estimates
+        """
+        xnew, Pnew, u = self.model_predition_step(u=u)
+        self.update_xPuK(xnew, Pnew, u, None)
+        xnew, Pnew, K = self.measurement_update_step(z=z, postcovkwargs=postcovkwargs)
+        self.update_xPuK(xnew, Pnew, None, K)
+    
+        return self.x[-1], self.P[-1]
+    
 
 
 __namespace__ = 'stateest'
